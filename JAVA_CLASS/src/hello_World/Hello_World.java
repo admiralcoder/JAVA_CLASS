9461c6ea498ed3d21cf284d042aefd91c2450834
admiralcoder <djale2008@icloud.com>
2018-03-18 01:17:12 -0400

    Uploading Java Classes

---
+++ JAVA_CLASS/src/hello_World/Hello_World.java
@@ -0,0 +1,25 @@
+package hello_World;
+
+public class Hello_World {
+	/**
+	 * Printing Hello World Message 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		
+		// <Option1>
+		System.out.println("Hello World!");
+		
+		// <Option2>
+		String hello = "Hello World!";
+		System.out.println(hello);
+		
+		// <Option3>
+		String world;
+		world = "Hello World!";
+		System.out.println(world);
+		
+	}
+
+}
---
+++ JAVA_CLASS/src/if_Statements/IF_Statement.java
@@ -0,0 +1,10 @@
+package if_Statements;
+
+public class IF_Statement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
